ce3a63d6ffe8061be8a2268527a546f9a70b6e6f
dianagsgs <deiaeneade@gmail.com>
2025-05-05 22:20:01 -0600

    change bitrate

--- npaw.js
+++ npaw.js
@@ -2,4 +2,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let npawPlugin = new NpawPlugin('powerce');
     let player = videojs('my-video');
     npawPlugin.registerAdapter(player, 'npaw_adapter.json');
+
+    const radios = document.querySelectorAll('input[name="bitrate"]');
+
+    radios.forEach(radio => {
+        radio.addEventListener('change', (event) => {
+        if (event.target.checked) {
+            console.log("Selected bitrate unit:", event.target.value);
+        }
+        });
+    });
 });
