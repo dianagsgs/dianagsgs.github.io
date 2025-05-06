3e37a694079aa6e449efbbf621e640a57c83b63c
dianagsgs <deiaeneade@gmail.com>
2025-05-05 19:41:05 -0600

    try now

--- npaw.js
+++ npaw.js
@@ -1,8 +1,7 @@
-import videojs from 'video.js';
-
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log("content loaded!");
     let npawPlugin = new NpawPlugin('powerce');
+    // let player = document.getElementById('my-video');
     let player = videojs('my-video');
     npawPlugin.registerAdapter(player, 'https://qa-smartplugin.youbora.com/catalog/v7/js/lib/last-build/samples/adapters-json/video/videojs/7.0.5-videojs-js.json');
   });
