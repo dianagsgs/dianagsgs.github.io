9f7226d23ff05b501fa005462d793988e15c80b1
dianagsgs <deiaeneade@gmail.com>
2025-05-05 21:43:18 -0600

    add adapter

--- npaw.js
+++ npaw.js
@@ -1,8 +1,5 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log("content loaded!");
     let npawPlugin = new NpawPlugin('powerce');
-    // let player = document.getElementById('my-video');
     let player = videojs('my-video');
-    npawPlugin.registerAdapter(player, 'https://qa-smartplugin.youbora.com/catalog/v7/js/lib/last-build/samples/adapters-json/video/videojs/7.0.5-videojs-js.json');
-  });
-  console.log("hola!");
+    npawPlugin.registerAdapter(player, 'npaw_adapter.json');
+});
