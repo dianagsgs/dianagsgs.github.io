cc805b037727915cc6505c047e33ffdcf9102a64
dianagsgs <deiaeneade@gmail.com>
2025-05-07 18:38:41 -0600

    initial adapter

--- npaw.js
+++ npaw.js
@@ -1,7 +1,9 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    const bits_json = 'http://qa-smartplugin.youbora.com/catalog/v7/js/lib/last-build/samples/adapters-json/video/videojs/7.0.5-videojs-js.json';
+    const mbps_json = 'npaw_adapter.json';
     let npawPlugin = new NpawPlugin('powerce');
     let player = videojs('my-video');
-    
+    npawPlugin.registerAdapter(player, bits_json);
 
     const radios = document.querySelectorAll('input[name="bitrate"]');
     radios.forEach(radio => {
@@ -10,10 +12,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
             console.log("Selected bitrate unit:", event.target.value);
             if(event.target.value === "bps"){
                 npawPlugin.removeAdapter("my-video");
-                npawPlugin.registerAdapter(player, 'http://qa-smartplugin.youbora.com/catalog/v7/js/lib/last-build/samples/adapters-json/video/videojs/7.0.5-videojs-js.json');
+                npawPlugin.registerAdapter(player, bits_json);
             } else { // event.target.value === "Mbps"
                 npawPlugin.removeAdapter("my-video");
-                npawPlugin.registerAdapter(player, 'npaw_adapter.json');
+                npawPlugin.registerAdapter(player, mbps_json);
             }
         }
         });
