c043d13ddc8f6a6b2b8786d115ca38f339963077
dianagsgs <deiaeneade@gmail.com>
2025-05-07 18:35:01 -0600

    toggle adapter code

--- npaw.js
+++ npaw.js
@@ -1,14 +1,20 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     let npawPlugin = new NpawPlugin('powerce');
     let player = videojs('my-video');
-    npawPlugin.registerAdapter(player, 'npaw_adapter.json');
+    
 
     const radios = document.querySelectorAll('input[name="bitrate"]');
-
     radios.forEach(radio => {
         radio.addEventListener('change', (event) => {
         if (event.target.checked) {
             console.log("Selected bitrate unit:", event.target.value);
+            if(event.target.value === "bps"){
+                npawPlugin.removeAdapter("my-video");
+                npawPlugin.registerAdapter(player, 'http://qa-smartplugin.youbora.com/catalog/v7/js/lib/last-build/samples/adapters-json/video/videojs/7.0.5-videojs-js.json');
+            } else { // event.target.value === "Mbps"
+                npawPlugin.removeAdapter("my-video");
+                npawPlugin.registerAdapter(player, 'npaw_adapter.json');
+            }
         }
         });
     });
